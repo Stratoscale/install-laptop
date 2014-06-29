5163fec29cae1ae492a84168e862aa1823b8d8ee
Shlomi Matichin <shlomi@stratoscale.com>
2014-06-29 22:31:40 +0300

    shell script based installer

--- Makefile
+++ Makefile
@@ -1,4 +1,40 @@
-install: install_osmosis install_upseto_and_solvent install_inaugurator install_yumcache install_rackattack_virtual
+all: install_here build test
+
+test: build
+	cd build/install-laptop ; ./install.sh
+
+.PHONY: build
+build: build/installer/install-laptop.tgz
+
+build/installer/install-laptop.tgz:
+	-rm -fr build/install-laptop*
+	-mkdir -p build/install-laptop/packages
+	-mkdir -p build/install-laptop/etc
+	-mkdir -p build/installer
+	cp ../osmosis/build/cpp/osmosis.bin build/install-laptop/packages -a
+	cp ../osmosis/osmosis.service build/install-laptop/etc -a
+	cp ../upseto/dist/*.egg build/install-laptop/packages -a
+	cp ../upseto/upseto.pth build/install-laptop/etc -a
+	cp ../upseto/upseto.sh build/install-laptop/etc -a
+	cp ../solvent/dist/*.egg build/install-laptop/packages -a
+	cp ../solvent/solvent.sh build/install-laptop/etc -a
+	cp solvent.conf build/install-laptop/etc -a
+	cp ../inaugurator/dist/*.egg build/install-laptop/packages -a
+	cp ../inaugurator/build/inaugurator.initrd.img ../inaugurator/build/inaugurator.vmlinuz build/install-laptop/packages -a
+	cp ../yumcache/dist/*.egg build/install-laptop/packages -a
+	cp ../yumcache/yumcache.service build/install-laptop/etc/ -a
+	cp yumcache.config build/install-laptop/etc -a
+	cp ../rackattack-virtual/build/*.egg build/install-laptop/packages -a
+	cp ../rackattack-virtual/rackattack-virtual.service build/install-laptop/etc -a
+	cp install.sh build/install-laptop/ -a
+	chmod 755 build/install-laptop/install.sh
+	tar -czf $@ -C build install-laptop
+
+submit: build/installer/install-laptop.tgz
+	solvent submitproduct installer `dirname $<`
+
+.PHONY: install_here
+install_here: install_osmosis install_upseto_and_solvent install_inaugurator install_yumcache install_rackattack_virtual
 
 install_osmosis:
 	cd ../osmosis ; DONT_START_SERVICE=yes make install
---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+echo INSTALL-LAPTOP
+
+#osmosis
+sudo systemctl stop osmosis
+sudo cp packages/osmosis.bin /usr/bin/osmosis
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/osmosis.service /usr/lib/systemd/system/osmosis.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl enable osmosis.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl start osmosis.service
+
+#upseto
+yes | sudo pip uninstall upseto
+sudo easy_install packages/upseto*.egg
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/upseto.pth /usr/lib/python2.7/site-packages/upseto.pth
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/upseto.sh /usr/bin/upseto
+if [ "$?" != "0" ]; then exit -1; fi
+sudo chmod 755 /usr/bin/upseto
+if [ "$?" != "0" ]; then exit -1; fi
+
+#solvent
+yes | sudo pip uninstall solvent
+sudo easy_install packages/solvent*.egg
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/solvent.sh /usr/bin/solvent
+if [ "$?" != "0" ]; then exit -1; fi
+sudo chmod 755 /usr/bin/solvent
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/solvent.conf /etc/solvent.conf -a
+if [ "$?" != "0" ]; then exit -1; fi
+
+#inaugurator
+sudo mkdir /usr/share/inaugurator
+yes | sudo pip uninstall inaugurator
+sudo easy_install packages/inaugurator*.egg
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp packages/inaugurator.initrd.img packages/inaugurator.vmlinuz /usr/share/inaugurator
+if [ "$?" != "0" ]; then exit -1; fi
+sudo chmod 644 /usr/share/inaugurator/*
+if [ "$?" != "0" ]; then exit -1; fi
+
+#yumcache
+sudo systemctl stop yumcache
+yes | sudo pip uninstall yumcache
+sudo easy_install packages/yumcache*.egg
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/yumcache.service /usr/lib/systemd/system/yumcache.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/yumcache.config /etc/yumcache.config -a
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl enable yumcache.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl start yumcache
+
+#rackattack-virtual
+sudo systemctl stop rackattack-virtual.service
+sudo mkdir /usr/share/rackattack.virtual
+sudo cp packages/rackattack.virtual.egg /usr/share/rackattack.virtual
+if [ "$?" != "0" ]; then exit -1; fi
+sudo cp etc/rackattack-virtual.service /usr/lib/systemd/system/rackattack-virtual.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl enable rackattack-virtual.service
+if [ "$?" != "0" ]; then exit -1; fi
+sudo systemctl start rackattack-virtual
+
+echo "Install-laptop Completed SUCCESSFULLY"
