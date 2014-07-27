b02e6e7edcf3a98563ffaf8f945d3bcdd125d067
Shlomi Matichin <shlomi@stratoscale.com>
2014-07-27 09:14:15 +0300

    install.sh will not use sudo if already root

--- install.sh
+++ install.sh
@@ -1,74 +1,78 @@
 #!/bin/sh
 echo INSTALL-LAPTOP
+SUDO=sudo
+if [ "$UID" == "0" ]; then
+    SUDO=
+fi
 
 #osmosis
-sudo systemctl stop osmosis
-sudo cp packages/osmosis.bin /usr/bin/osmosis
+$SUDO systemctl stop osmosis
+$SUDO cp packages/osmosis.bin /usr/bin/osmosis
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/osmosis.service /usr/lib/systemd/system/osmosis.service
+$SUDO cp etc/osmosis.service /usr/lib/systemd/system/osmosis.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl enable osmosis.service
+$SUDO systemctl enable osmosis.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl start osmosis.service
+$SUDO systemctl start osmosis.service
 
 #upseto
-yes | sudo pip uninstall upseto
-sudo easy_install packages/upseto*.egg
+yes | $SUDO pip uninstall upseto
+$SUDO easy_install packages/upseto*.egg
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/upseto.pth /usr/lib/python2.7/site-packages/upseto.pth
+$SUDO cp etc/upseto.pth /usr/lib/python2.7/site-packages/upseto.pth
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/upseto.sh /usr/bin/upseto
+$SUDO cp etc/upseto.sh /usr/bin/upseto
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/upseto.bash.completion.sh /etc/bash_completion.d/upseto.sh
+$SUDO cp etc/upseto.bash.completion.sh /etc/bash_completion.d/upseto.sh
 if [ "$?" != "0" ]; then exit -1; fi
-sudo chmod 755 /usr/bin/upseto
+$SUDO chmod 755 /usr/bin/upseto
 if [ "$?" != "0" ]; then exit -1; fi
 
 #solvent
-yes | sudo pip uninstall solvent
-sudo easy_install packages/solvent*.egg
+yes | $SUDO pip uninstall solvent
+$SUDO easy_install packages/solvent*.egg
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/solvent.sh /usr/bin/solvent
+$SUDO cp etc/solvent.sh /usr/bin/solvent
 if [ "$?" != "0" ]; then exit -1; fi
-sudo chmod 755 /usr/bin/solvent
+$SUDO chmod 755 /usr/bin/solvent
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/solvent.bash.completion.sh /etc/bash_completion.d/solvent.sh
+$SUDO cp etc/solvent.bash.completion.sh /etc/bash_completion.d/solvent.sh
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/solvent.conf /etc/solvent.conf -a
+$SUDO cp etc/solvent.conf /etc/solvent.conf -a
 if [ "$?" != "0" ]; then exit -1; fi
 
 #inaugurator
-sudo mkdir /usr/share/inaugurator
-yes | sudo pip uninstall inaugurator
-sudo easy_install packages/inaugurator*.egg
+$SUDO mkdir /usr/share/inaugurator
+yes | $SUDO pip uninstall inaugurator
+$SUDO easy_install packages/inaugurator*.egg
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp packages/inaugurator.initrd.img packages/inaugurator.vmlinuz /usr/share/inaugurator
+$SUDO cp packages/inaugurator.initrd.img packages/inaugurator.vmlinuz /usr/share/inaugurator
 if [ "$?" != "0" ]; then exit -1; fi
-sudo chmod 644 /usr/share/inaugurator/*
+$SUDO chmod 644 /usr/share/inaugurator/*
 if [ "$?" != "0" ]; then exit -1; fi
 
 #yumcache
-sudo systemctl stop yumcache
-yes | sudo pip uninstall yumcache
-sudo easy_install packages/yumcache*.egg
+$SUDO systemctl stop yumcache
+yes | $SUDO pip uninstall yumcache
+$SUDO easy_install packages/yumcache*.egg
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/yumcache.service /usr/lib/systemd/system/yumcache.service
+$SUDO cp etc/yumcache.service /usr/lib/systemd/system/yumcache.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/yumcache.config /etc/yumcache.config -a
+$SUDO cp etc/yumcache.config /etc/yumcache.config -a
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl enable yumcache.service
+$SUDO systemctl enable yumcache.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl start yumcache
+$SUDO systemctl start yumcache
 
 #rackattack-virtual
-sudo systemctl stop rackattack-virtual.service
-sudo mkdir /usr/share/rackattack.virtual
-sudo cp packages/rackattack.virtual.egg /usr/share/rackattack.virtual
+$SUDO systemctl stop rackattack-virtual.service
+$SUDO mkdir /usr/share/rackattack.virtual
+$SUDO cp packages/rackattack.virtual.egg /usr/share/rackattack.virtual
 if [ "$?" != "0" ]; then exit -1; fi
-sudo cp etc/rackattack-virtual.service /usr/lib/systemd/system/rackattack-virtual.service
+$SUDO cp etc/rackattack-virtual.service /usr/lib/systemd/system/rackattack-virtual.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl enable rackattack-virtual.service
+$SUDO systemctl enable rackattack-virtual.service
 if [ "$?" != "0" ]; then exit -1; fi
-sudo systemctl start rackattack-virtual
+$SUDO systemctl start rackattack-virtual
 
 echo "Install-laptop Completed SUCCESSFULLY"
