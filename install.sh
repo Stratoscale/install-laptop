4e4a83f06e239581f27c554c457c4a69a4ade2e6
Eliran Levi <eliran@stratoscale.com>
2015-05-31 15:46:19 +0000

    Remove asset.conf

--- install.sh
+++ install.sh
@@ -86,10 +86,4 @@ if [ "$?" != "0" ]; then exit -1; fi
 $SUDO chmod 755 /usr/bin/logbeam
 if [ "$?" != "0" ]; then exit -1; fi
 
-#configure asset
-if [ ! -x /etc/asset.conf ]; then
-    $SUDO cp etc/asset.conf /etc/asset.conf -a
-    if [ "$?" != "0" ]; then exit -1; fi
-fi
-
 echo "Install-laptop Completed SUCCESSFULLY"
